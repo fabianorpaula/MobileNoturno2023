9727021bf2bc4a304f034a6798109fadcf9dce15
Fabiano De Paula <fabiano.rpaula@gmail.com>
2023-08-04 22:15:48 -0300

    Initial commit

---
+++ Assets/Scripts/GerenciadorDeObjetos.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerenciadorDeObjetos : MonoBehaviour
+{
+    public List<GameObject> MinhasEsferas;
+    public GameObject Bola;
+    public float meuTempo;
+
+    void Update()
+    {
+        //CriarBola();
+        CriarBolaDificil();
+    }
+
+    void CriarBola()
+    {
+        meuTempo += Time.deltaTime;
+        if (meuTempo > 1)
+        {
+            float posX = Random.Range(-2, 2.1f);
+            Vector3 novaPos = new Vector3(posX, 6, 0);
+            int tipoEsfera = Random.Range(0, 4);
+            GameObject Bol = Instantiate(MinhasEsferas[tipoEsfera], novaPos, Quaternion.identity);
+            Destroy(Bol, 2f);
+            meuTempo = 0;
+        }
+    }
+
+    void CriarBolaDificil()
+    {
+        meuTempo += Time.deltaTime;
+        if (meuTempo > 1)
+        {
+            float posX = Random.Range(-2, 2.1f);
+            Vector3 novaPos = new Vector3(posX, 6, 0);
+            int esferaEscolhida = 0;
+            int tipoEsfera = Random.Range(0, 101);
+            if (tipoEsfera < 80)
+            {
+                esferaEscolhida = 0;
+            }
+            else if(tipoEsfera < 90)
+            {
+                esferaEscolhida = 1;
+            }
+            else if (tipoEsfera < 98)
+            {
+                esferaEscolhida = 2;
+            }
+            else
+            {
+                esferaEscolhida = 3;
+            }
+            GameObject Bol = Instantiate(MinhasEsferas[esferaEscolhida], novaPos, Quaternion.identity);
+            Destroy(Bol, 2f);
+            meuTempo = 0;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MobileNoturno2023
+ 
