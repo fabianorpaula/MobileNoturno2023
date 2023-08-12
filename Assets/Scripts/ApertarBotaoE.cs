3289c08e6c0ac49a6e63aeb539eb4f4d4351a7b0
Fabiano De Paula <fabiano.rpaula@gmail.com>
2023-08-11 21:15:53 -0300

    Mover Botão e Dedo

---
+++ Assets/Scripts/ApertarBotaoE.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ApertarBotaoE : Button
+{
+    private GameObject Personagem;
+    void Start()
+    {
+        Personagem = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        Pressionar();
+    }
+
+    void Pressionar()
+    {
+        if (IsPressed() == true)
+        {
+            Personagem.GetComponent<Movimento>().MoverE();
+        }
+    }
+}
---
+++ Assets/Scripts/Movimento.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    private float tempo;
+    
+    void Update()
+    {
+        tempo += Time.deltaTime;
+        if(tempo > 0.001f)
+        {
+            Mover();
+        }
+        
+        
+
+    }
+
+    void Mover()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //Captura a Posição do Mouse
+            Vector3 destino = Input.mousePosition;
+            //Corrigir Posição
+            Vector3 desCorri = Camera.main.ScreenToWorldPoint(destino);
+            //Destino Final Corrigido
+            Vector3 dFinal = new Vector3(desCorri.x, -3f, 0);
+            //Mover Objeto
+            transform.position = Vector3.MoveTowards(transform.position, dFinal, 0.1f);
+
+        }
+    }
+
+
+    public void MoverD()
+    {
+        Debug.Log("Mandou Mover D");
+    }
+    public void MoverE()
+    {
+        Debug.Log("Mandou Mover E");
+    }
+}
